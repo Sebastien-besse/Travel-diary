c91e142459b353db828d4e149e44813b535ce32b
Sébastien <sebbody79400@gmail.com>
2024-12-05 19:32:33 +0100

    Initial commit

---
+++ Travel-diary/Travel_diaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_diaryApp.swift
+//  Travel-diary
+//
+//  Created by Sebastien Besse on 05/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Travel_diaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
