2240f1b76631333cf919d8d301ab390a8fccca22
Sébastien <sebbody79400@gmail.com>
2024-12-05 19:46:59 +0100

    Created Model

---
+++ Travel-diary/Model/Memory.swift
@@ -0,0 +1,19 @@
+//
+//  Memory.swift
+//  Travel-diary
+//
+//  Created by Sebastien Besse on 05/12/2024.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Memory: Identifiable{
+    let id = UUID()
+    let title: String
+    let image: Image?
+    let note: Int
+    let description: String
+    var localisation: CLLocationCoordinate2D?
+}
